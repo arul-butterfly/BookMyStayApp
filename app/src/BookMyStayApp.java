d68c40af0b68677c5c16925af702037fcdb21ada
as1167-rgb <as1167@srmist.edu.in>
2026-03-27 14:58:09 +0530

    arul Added UC1

---
+++ app/src/BookMyStayApp.java
@@ -0,0 +1,15 @@
+
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=======================================");
+        System.out.println("   Welcome to Book My Stay App");
+        System.out.println("   Hotel Booking System v1.0");
+        System.out.println("=======================================");
+
+        System.out.println("Your comfort, our priority!");
+
+        System.out.println("Application started and terminated successfully.");
+    }
+}
